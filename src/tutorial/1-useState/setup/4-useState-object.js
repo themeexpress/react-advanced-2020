c8ad15d1419d01e704d2d68d970661b9c2be5d30
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2021-10-24 14:56:14 +0900

    UseState in object
    Accessing Object by using useState()

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
-
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name : 'Peter',
+    age : 32,
+    message : 'Random message',
+  });
+  const [name, setName] = useState('Peter');
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState('Random messag');
+  const changeMessage = () => {
+    //setPerson({...person, message:  "Hello Wolrd"});
+    setMessage('Hello ReactJs');
+  }
+  return (
+  <>
+    <h3>{name}</h3>
+    <h3>{age}</h3>
+    <h3>{message}</h3>
+    <button type="button" className="btn" onClick={changeMessage}> Change Message </button>
+  </>
+  )
 };
 
 export default UseStateObject;
