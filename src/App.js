20168696c71c93c0ba3b741749d02aa81cef6861
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2021-11-14 03:10:08 +0900

    Fetch Data
    Fetch data by using useEffect

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import React from 'react'
 //import Setup from './tutorial/1-useState/setup/4-useState-object';
 //import Setup from './tutorial/1-useState/setup/5-useState-counter';
 // import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
-import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
+// import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
+import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
+
 function App() {
   return (
     <div className='container'>
