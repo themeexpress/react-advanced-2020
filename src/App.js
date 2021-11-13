606c664f6042fb3bd0f195d60bee17f56b2a64db
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2021-11-13 18:49:16 +0900

    useEffect
    Basics of useEffecg

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react'
 //import Setup from './tutorial/1-useState/setup/2-useState-basics';
 //import Setup from './tutorial/1-useState/setup/2-useState-array';
 //import Setup from './tutorial/1-useState/setup/4-useState-object';
-import Setup from './tutorial/1-useState/setup/5-useState-counter';
+//import Setup from './tutorial/1-useState/setup/5-useState-counter';
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
 function App() {
   return (
     <div className='container'>
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,8 +2,29 @@ import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
+
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log('Call the useEffect' + value);
+    if(value >= 1){
+      document.title = `New Messages(${value})`;
+    }
+  }, [value]);
+  //We can use useEffect many times and dependency (Array) will be invoke only. So other useEffect will not affected
+  useEffect(() => {
+    console.log('Call the useEffect' + value);
+    if(value >= 1){
+      document.title = `New Messages(${value})`;
+    }
+  }, []);
+  console.log('Render the Component' + value);
+  return (
+  <>
+    <h2>{ value }</h2>
+    <button className='btn' onClick={() => setValue(value + 1)}>ClickMe</button>
+  </>
+  );
 };
 
 export default UseEffectBasics;
