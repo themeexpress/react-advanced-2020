f63ec895e43816784b44a1fa603cf0519a444cd7
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2021-11-27 16:51:45 +0900

    conditional rendering
    conditional rendering

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import React from 'react'
 //import Setup from './tutorial/1-useState/setup/5-useState-counter';
 // import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
 // import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
-import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
+// import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
+import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
 
 function App() {
   return (
