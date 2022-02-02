2f2e93a2d10eb84a9228182b33633745809fb232
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2022-02-02 23:19:09 +0900

    Multiple input control
    Dynamic Object in JS and React

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
-//import Setup from './tutorial/1-useState/setup/1-error-example';
-//import Setup from './tutorial/1-useState/setup/2-useState-basics';
-//import Setup from './tutorial/1-useState/setup/2-useState-array';
-//import Setup from './tutorial/1-useState/setup/4-useState-object';
-//import Setup from './tutorial/1-useState/setup/5-useState-counter';
+// import Setup from './tutorial/1-useState/setup/1-error-example';
+// import Setup from './tutorial/1-useState/setup/2-useState-basics';
+// import Setup from './tutorial/1-useState/setup/2-useState-array';
+// import Setup from './tutorial/1-useState/setup/4-useState-object';
+// import Setup from './tutorial/1-useState/setup/5-useState-counter';
 // import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
 // import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
 // import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
 // import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
 // import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit';
-import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
+// import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
+// import Setup from './tutorial/4-forms/setup/1-controlled-inputs';
+import Setup from './tutorial/4-forms/setup/2-multiple-inputs';
 
 function App() {
   return (
--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,58 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState('')
+  const [email, setEmail] = useState('')
+  const [address, setAddress] = useState('')
+  const [people, setPeople] = useState([])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(firstName && email && address){
+      const person = {id: new Date().getTime().toString(), firstName, email, address };
+      setPeople((people)=>{
+        return  [...people, person]
+      });
+      console.log(people);
+      //setFirstName('');
+      //setEmail('');
+      //setAddress('');
+    }
+    else{
+
+    }
+  }
+  return (
+    <>
+      <article>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor='firstName'>Name : </label>
+            <input type='text' id='firstName' name='firstName' value={firstName} onChange={(e)=>setFirstName(e.target.value)}/>
+          </div>
+          <div className='form-control'>
+            <label htmlFor='email'>Email : </label>
+            <input type='text' id='email' name='email' value={email} onChange={(e)=>setEmail(e.target.value)} />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='address'>Address : </label>
+            <input type='text' id='address' name='address' value={address} onChange={(e)=>setAddress(e.target.value)}/>
+          </div>
+          <button type='submit'>Add Person</button>
+        </form> 
+        {
+          people.map((person) => {
+            const{ id, firstName, email, address} = person
+            return (
+            <div className='item' key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+              <p>{address}</p>
+            </div>
+            );
+          })}       
+      </article>    
+    </>
+  );
 };
 
 export default ControlledInputs;
